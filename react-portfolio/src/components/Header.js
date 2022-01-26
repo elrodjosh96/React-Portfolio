24d053c63d3a8d95b4011b2b413f73a46c9af5a4
Josh Elrod <elrodjosh96@gmail.com>
2022-01-25 21:05:37 -0500

    header added

---
+++ react-portfolio/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return <div className='header-wrapper'>
+      <div className='main-info'>
+          <h1>WELCOME TO MY PORTFOLIO!</h1>
+      </div>
+  </div>;
+};
+
+export default Header;
--- react-portfolio/src/components/Navbar.js
+++ react-portfolio/src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
             <div className="container-fluid">
                 <div className="container">
-                    <a className="navbar-brand" href="#">HELLO THERE!</a>
+                    <a className="navbar-brand" href="#"></a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
