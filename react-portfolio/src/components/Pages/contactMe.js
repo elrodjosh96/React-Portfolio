c9d90910c8549795ecc414a41a073fb2bb6b21dd
Josh Elrod <elrodjosh96@gmail.com>
2022-01-28 20:34:39 -0500

    contact me form sends emails from site!

--- react-portfolio/src/components/Pages/contactMe.js
+++ react-portfolio/src/components/Pages/contactMe.js
@@ -3,87 +3,105 @@ import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import emailjs, { send } from '@emailjs/browser'
 
+
+
 export default function Contact() {
 
-        const formik = useFormik({
-            initialValues: {
-                firstName: '',
-                lastName: '',
-                subject: '',
-                message: '',
-                email: '',
-                to_name: 'elrodjosh96@gmail.com'
-            },
-            validationSchema: Yup.object({
-                firstName: Yup.string().max(15, '15 or less').required('Required'),
-                lastName: Yup.string().max(15, '15 or less').required('Required'),
-                message: Yup.string().message().required('Required'),
-                email: Yup.string().email('Invalid email').required('Required')
-            }),
-            onSubmit: (values) => {
-                console.log(values)
+    const formik = useFormik({
+        initialValues: {
+            reply_to: '',
+            subject: '',
+            message: '',
+            to_name: 'elrodjosh96@gmail.com'
+        },
+        validationSchema: Yup.object({
+            reply_to: Yup.string().email('Invalid email').required('Required'),
+            subject: Yup.string().max(20, '20 or less').required('Required'),
+            message: Yup.string().required('Required'),
+        }),
+
+        onSubmit: (values) => {
+            const serviceID = 'service_ubjz8ut';
+            const templateID = 'template_0bsdsad';
+            const userID = 'user_p2HjOuQpvTl4iEXUHAS70';
+
+            try {
+                emailjs.send(serviceID, templateID, values, userID)
+                    .then(() => {
+                        console.log('Success!')
+                        alert('Thank you for your email! I will respond as soon as I can! Have a great day!')
+                    })
             }
-        })
-        console.log(formik.values);
-        return (
-            <form onSubmit={formik.handleSubmit}>
-                <h1>You can send me an email here for any further questions!</h1>
-                <div className='input-container'>
-                    <input
-                        id='firstName'
-                        name='firstName'
-                        type='text'
-                        placeholder='First Name'
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        value={formik.values.firstName}
-                    />
-                    {formik.touched.firstName && formik.errors.firstName ? <p>{formik.errors.firstName}</p> : null}
-                </div>
-                <div className='input-container'>
-                    <input
-                        id='lastName'
-                        name='lastName'
-                        type='text'
-                        placeholder='Last Name'
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        value={formik.values.lastName} />
-                        {formik.touched.lastName && formik.errors.lastName ? <p>{formik.errors.lastName}</p> : null}
-                </div>
-                <div className='input-container'>
-                    <input
-                        id='subject'
-                        name='subject'
-                        type='text'
-                        placeholder='Subject'
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        value={formik.values.subject} />
-                        {formik.touched.subject && formik.errors.subject ? <p>{formik.errors.subject}</p> : null}
-                </div>
+            catch {
+                console.log('ERROR')
+            }
+        }
+    });
+        console.log(formik.values)
+    return (
+
+        <form onSubmit={formik.handleSubmit}>
+            <h1>You can send me an email here for any further questions!</h1>
+            <div className='input-container'>
                 <input
-                        id='message'
-                        name='message'
-                        type='text'
-                        placeholder='Type Your Message Here'
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        value={formik.values.message}
-                    />
-                <div className='input-container'>
-                    <input
-                        id='email'
-                        name='email'
-                        type='text'
-                        placeholder='Email'
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        value={formik.values.email} />
-                        {formik.touched.email && formik.errors.email ? <p>{formik.errors.email}</p> : null}
-                </div>
-                <button type='submit'>Submit</button>
-            </form>
+                    id='reply_to'
+                    name='reply_to'
+                    onChange={formik.handleChange}
+                    type='reply_to'
+                    placeholder='Your Email'
+                    onBlur={formik.handleBlur}
+                    value={formik.values.reply_to}
+                />
+                {formik.touched.reply_to && formik.errors.reply_to ? <p>{formik.errors.reply_to}</p> : null}
+            </div>
+            <div className='input-container'>
+                <input
+                    id='subject'
+                    name='subject'
+                    onChange={formik.handleChange}
+                    type='subject'
+                    placeholder='Subject'
+                    onBlur={formik.handleBlur}
+                    value={formik.values.subject}
+                />
+                {formik.touched.subject && formik.errors.subject ? <p>{formik.errors.subject}</p> : null}
+            </div>
+            <div className='input-container'>
+                <input
+                    id='message'
+                    name='message'
+                    onChange={formik.handleChange}
+                    type='message'
+                    placeholder='Please type your message here.'
+                    onBlur={formik.handleBlur}
+                    value={formik.values.message}
+                />
+                {formik.touched.message && formik.errors.message ? <p>{formik.errors.message}</p> : null}
+            </div>
+            {/* <div>
+                <input
+                    id='message'
+                    name='message'
+                    type='text'
+                    placeholder='Type Your Message Here'
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.message}
+                />
+            </div>
+            <div className='input-container'>
+                <input
+                    id='reply_to'
+                    name='reply_to'
+                    type='text'
+                    placeholder='Reply To'
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.reply_to} />
+                {formik.touched.reply_to && formik.errors.reply_to ? <p>{formik.errors.reply_to}</p> : null}
+            </div> */}
+            <button type='submit'>Submit</button>
+        </form>
     )
 }
 // import React from 'react';
