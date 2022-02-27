a7bfbb058bd2545ae8a54d976f7d61a0a6492585
Josh Elrod <elrodjosh96@gmail.com>
2022-02-26 22:06:33 -0500

    fixed resume again

--- react-portfolio/src/components/Pages/Resume.js
+++ react-portfolio/src/components/Pages/Resume.js
@@ -4,7 +4,7 @@ import '../css/resume.css';
 
 export default function Resume ({ currPage, setCurrPage }) {
     return <div className='container resume-section'>
-        <iframe title='resume-pdf' src="https://drive.google.com/file/d/1CfMofNKPq0iR8fYr0o5pjsgRCpCDLzEM/preview" width="90%" allow="autoplay"></iframe>
+        <iframe title='resume-pdf' src="https://drive.google.com/file/d/1VdevJjS0qelhZnpJq6u1VT6mAFJSqLxY/preview" width="90%" allow="autoplay"></iframe>
         <br/>
         <br/>
         {/* <a className='resume-link' href='https://drive.google.com/file/d/19-d2Pomi8aj1vGR8OdNrUnvcnn2s16iZ/preview'>You can download a copy of My Resume here!</a> */}
