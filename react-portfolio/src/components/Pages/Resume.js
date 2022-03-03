3d7094bbbe0ef5e2428e72e0f49d2761d244fe1f
Josh Elrod <elrodjosh96@gmail.com>
2022-03-03 16:18:35 -0500

    resume updates

--- react-portfolio/src/components/Pages/Resume.js
+++ react-portfolio/src/components/Pages/Resume.js
@@ -4,7 +4,7 @@ import '../css/resume.css';
 
 export default function Resume ({ currPage, setCurrPage }) {
     return <div className='container resume-section'>
-        <iframe title='resume-pdf' src="https://drive.google.com/file/d/1CKMgmwrODC4hCp4qjmDjV3z5qa-eGKZ6/preview" width="90%" allow="autoplay"></iframe>
+        <iframe title='resume-pdf' src="https://drive.google.com/file/d/12U9cpT6cInnz9a7SvJq59sRoR48PaRCM/preview" width="90%" allow="autoplay"></iframe>
         <br/>
         <br/>
         {/* <a className='resume-link' href='https://drive.google.com/file/d/19-d2Pomi8aj1vGR8OdNrUnvcnn2s16iZ/preview'>You can download a copy of My Resume here!</a> */}
